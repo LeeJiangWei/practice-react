0744f1ffbe94f8fe59c78fe3c02c75370ffbae85
Lee Jiangwei <ljwskrskr@outlook.com>
2019-07-11 22:33:10 +0800

    Implement ApprovalCard and using CommentDetail as childen

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from "./ApprovalCard"
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author={faker.name.lastName() + faker.name.findName()}
-                time={faker.date.past().toLocaleDateString()}
-                content={faker.lorem.sentences()}
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author={faker.name.lastName() + faker.name.findName()}
-                time={faker.date.past().toLocaleDateString()}
-                content={faker.lorem.sentences()}
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author={faker.name.lastName() + faker.name.findName()}
-                time={faker.date.past().toLocaleDateString()}
-                content={faker.lorem.sentences()}
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.lastName() + faker.name.findName()}
+                    time={faker.date.past().toLocaleDateString()}
+                    content={faker.lorem.sentences()}
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.lastName() + faker.name.findName()}
+                    time={faker.date.past().toLocaleDateString()}
+                    content={faker.lorem.sentences()}
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.lastName() + faker.name.findName()}
+                    time={faker.date.past().toLocaleDateString()}
+                    content={faker.lorem.sentences()}
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
