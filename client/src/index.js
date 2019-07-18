5c6884bb7fb15b15092db060150218636f6d07c0
Lee Jiangwei <ljwskrskr@outlook.com>
2019-07-18 16:41:15 +0800

    Add .gitignore

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reduxThunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose} from "redux";
 import { Provider } from 'react-redux';
 
@@ -8,7 +9,7 @@ import reducers from './reducers';
 
 // activate redux dev tool
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
     <Provider store={store}>
