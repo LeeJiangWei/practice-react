36c09cebc5c447a75e9673d6ec248f399ab3bdfa
Lee Jiangwei <ljwskrskr@outlook.com>
2019-07-20 11:36:21 +0800

    All done

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
-
     dispatch({
         type: "FETCH_POSTS",
         payload: response.data
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux";
 import { fetchPostsAndUsers } from "../actions";
+import {fetchUser} from "../actions";
 // import { fetchPosts } from "../actions";
 
 import UserHeader from './UserHeader';
